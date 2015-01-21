46981bf03451ee814ffcc1d439c55564423fdc7c
David Stanley <davidstanley01@gmail.com>
2015-01-21 11:55:00 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Introduction to Design Patterns
+======================================
+
+This talk was given at the January meeting of the PDX PHP User's Group.
---
+++ samples/DependencyInjection/AbstractConfig.php
@@ -0,0 +1,17 @@
+<?php
+namespace DesignPatterns\Structural\DependencyInjection;
+
+/**
+ * class AbstractConfig
+ */
+abstract class AbstractConfig
+{
+    /**
+     * @var Storage of data
+     */
+    protected $storage;
+    public function __construct($storage)
+    {
+        $this->storage = $storage;
+    }
+}
